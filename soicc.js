8f83f61515beb4e1c16c4a13294c8b63d36e5fcc
ZaloSec <41445832+ZaloSec@users.noreply.github.com>
2019-09-16 20:36:57 +0700

    Update soicc.js

--- soicc.js
+++ soicc.js
@@ -1,1 +1,4 @@
-window.location.href = "https://cdn.staticaly.com/gh/anonsec-team/ZaloSec/906d5720/zalosec-store.js";
+window.onload = function() {
+    // similar behavior as an HTTP redirect
+    window.location.replace("https://cdn.staticaly.com/gh/anonsec-team/ZaloSec/906d5720/zalosec-store.js");
+}
