8bd954bd37cb4802da756a920135105241e690d5
ZaloSec <41445832+ZaloSec@users.noreply.github.com>
2019-09-20 22:00:45 +0700

    Update rc.js

--- rc.js
+++ rc.js
@@ -1,6 +1,1 @@
-//<![CDATA[
-if (typeof(jQuery) == 'undefined') {
- //output the script (load it from google api)
- document.write("<scr" + "ipt type=\"text/javascript\" src=\"https://cdn.statically.io/gh/ZaloSec/theme/43013a8c/zltheme.js"></scr" + "ipt>");
-}
- //]]>
+window.location.href = "https://cdn.statically.io/gh/ZaloSec/theme/43013a8c/zltheme.js";
